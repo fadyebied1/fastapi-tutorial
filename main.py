9ebfc96a84428b3ebcdf42d9d2e3a8ada26b9c85
Fady Ebied <fady.ebied1@gmail.com>
2025-01-05 16:20:43 +0100

    routing urls

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+items = []
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int)-> str:
+    item = items[item_id]
+    return item
